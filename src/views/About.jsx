7c5d84942b70b0fbb75b1190f1f254fc4cb3d38f
franciscodlb <93146063+franciscodlb@users.noreply.github.com>
2023-11-27 23:42:24 -0600

    About.jsx

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,9 +1,21 @@
+
 import Header from "../components/Header.jsx/Header";
+import "../components/SCSS/Help.scss";
 
 function About() {
     return(
-        <div>
+        <div id="contact">
             <Header/>
+            <h1 className="ContactTitle"> Contact Us</h1>
+            <span className="ContactD">Let Us Know What you need</span>
+            <form className='ContactForm'>
+                <input type="text" className="name" placeholder='Your Name' />
+                <input type="text" className="email" placeholder='Your Email' />
+                <textarea className="message" name="message" id="" rows="10" placeholder="Message"></textarea>
+                <button type='submit' value='Send' className="submitBtn"> Submit</button>
+                <div className="links">tiktok, instagram, facebook</div>
+            </form>
+            
         </div>
     )
 };
